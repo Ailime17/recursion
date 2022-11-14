f194d91c2b8d0d621aa662c4d9f0b07df52d9c80
Emilia <emilia.skrzypczak.es@gmail.com>
2022-11-14 19:55:10 +0100

    Improve indentation

--- merge_sort/script.rb
+++ merge_sort/script.rb
@@ -2,22 +2,22 @@ def merge_sort(array)
   if array.length < 2
       array
   else
-      left_array = array[0...array.length/2]
-      right_array = array[array.length/2..-1]
-      sorted_left = merge_sort(left_array)
-      sorted_right = merge_sort(right_array)
+    left_array = array[0...array.length/2]
+    right_array = array[array.length/2..-1]
+    sorted_left = merge_sort(left_array)
+    sorted_right = merge_sort(right_array)
 
-      sorted_array = []
-      until sorted_left.empty? && sorted_right.empty?
+    sorted_array = []
+    until sorted_left.empty? && sorted_right.empty?
       if sorted_left.empty? 
-          return sorted_array + sorted_right
+        return sorted_array + sorted_right
       elsif sorted_right.empty?
-          return sorted_array + sorted_left
+        return sorted_array + sorted_left
       else
           sorted_left[0] < sorted_right[0] ? sorted_array << sorted_left.shift : sorted_array << sorted_right.shift
       end
-      end
-      sorted_array
+    end
+    sorted_array
   end
 end
 p merge_sort([1, 112, 3, 45, 6])
